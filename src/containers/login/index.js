5bd7bb5bf94f84a9256420cb17fd7d06c75d37bd
AndrTiger <andriisvirskyi@gmail.com>
2018-11-23 17:43:11 +0200

    global webpack, refactoring code

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,7 +1,7 @@
 import Authorization from './components/authorization/authorization'
 import PasswordRecovery from './components/passwordRecovery/passwordRecovery'
 import Registration from './components/registration/registration'
-import './login.css'
+import './login.scss'
 
 export {
     Authorization,
